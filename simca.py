1832bc458a93d04de852e44fc4b7abb808fd98ca
Lucas Kern <lucasgilbertokern@gmail.com>
2016-10-24 09:02:25 -0200

    Fixes and test for target error

--- simca.py
+++ simca.py
@@ -2,7 +2,7 @@ import sys
 from optparse import OptionParser
 from subprocess import call
 
-def sim(profile, fight, iterations=10000, scaleFactors=0, time=300, variance=0.1, bossCount=1, cores=4, outputFile=None, logFile=None, optimalRaid=True, disableBloodlust=False, plotStats=None, plotPoints=20, plotStep=160.0, xmlFile = None, jsonFile = None):
+def sim(profile, fight, iterations=10000, scaleFactors=0, time=300, variance=0.1, bossCount=1, cores=4, outputFile=None, logFile=None, optimalRaid=True, disableBloodlust=False, plotStats=None, plotPoints=20, plotStep=160.0, plotTargetError=0.1, xmlFile = None, jsonFile = None):
     print("simming!")
     +profile+fight+bossCount+str(time)
     arguments='iterations={0} calculate_scale_factors={1} max_time={2} vary_combat_length={3} fight_style={4} desired_targets={5} threads={6}'.format(iterations, scaleFactors, time, variance, fight, bossCount, cores)
@@ -10,6 +10,8 @@ def sim(profile, fight, iterations=10000, scaleFactors=0, time=300, variance=0.1
         arguments+=' override.bloodlust=0'
     if plotStats:
         arguments+=' dps_plot_stat={0} dps_plot_points={1} dps_plot_step={2}'.format(plotStats, plotPoints, plotStep)
+        if plotTargetError != 0.1:
+            arguments+=' dps_plot_target_error={0}'.format(plotTargetError) 
     if outputFile:
         arguments+=' html={0}.html'.format(outputFile+'.'+profile+'.'+fight+'.'+bossCount+'.'+time)
     if xmlFile:
@@ -19,7 +21,7 @@ def sim(profile, fight, iterations=10000, scaleFactors=0, time=300, variance=0.1
     if logFile:
        arguments+=' > {0}.log'.format(logFile)
     
-    call("simc.exe iterations={0} calculate_scale_factors={1} max_time={2} vary_combat_length={3} fight_style={4} desired_targets={5} threads={6} html={7}.html {8}".format(iterations, scaleFactors, time, variance, fight, bossCount, cores, outputFile, profile))
+    call("simc.exe "+arguments)
     
 def fight_reader(fight):
     fight = fight.lower()
@@ -57,6 +59,7 @@ def main():
     parser.add_option("-y", "--plot-stats", dest="plotStats", help="the DPS stats that should be plotted (supports comma-separated lists)", default=None)   
     parser.add_option("-z", "--plot-points", dest="plotPoints", help="the number of points to use to create the plot graph.", default=20)   
     parser.add_option("-e", "--plot-steps", dest="plotStep", help=" is the delta between two points of the plot graph.", default=160.0)   
+    parser.add_option("-a", "--plot-target-error", dest="plotTargetError", help=" hopefully this works!", default=0.1)   
     parser.add_option("-x", "--xml", dest="xmlFile", help="the xml output file name (base xml output file name for multiple simulations)", default=None) 
     parser.add_option("-j", "--json", dest="jsonFile", help="the json output file name (base json output file name for multiple simulations)", default=None) 
 
@@ -73,9 +76,9 @@ def main():
             for fight in options.fights.split(","):
                 if isinstance(options.bosses,str):
                     for bossCount in options.bosses.split(","):
-                        sim(profile, fight_reader(fight), options.iterations, 1 if options.scaleFactors else 0, time, options.variance, bossCount, options.cores, options.outputFile, options.logFile, options.optimalRaid, options.disableBloodlust, options.plotStats, options.plotPoints, options.plotStep, xmlFile, jsonFile)
+                        sim(profile, fight_reader(fight), options.iterations, 1 if options.scaleFactors else 0, time, options.variance, bossCount, options.cores, options.outputFile, options.logFile, options.optimalRaid, options.disableBloodlust, options.plotStats, options.plotPoints, options.plotStep, options.plotTargetError xmlFile, jsonFile)
                 else:
-                    sim(profile, fight_reader(fight), options.iterations, 1 if options.scaleFactors else 0, time, options.variance, options.bosses, options.cores, options.outputFile, options.logFile, options.optimalRaid, options.disableBloodlust, options.plotStats, options.plotPoints, options.plotStep, options.xmlFile, options.jsonFile)
+                    sim(profile, fight_reader(fight), options.iterations, 1 if options.scaleFactors else 0, time, options.variance, options.bosses, options.cores, options.outputFile, options.logFile, options.optimalRaid, options.disableBloodlust, options.plotStats, options.plotPoints, options.plotStep, options.plotTargetError, options.xmlFile, options.jsonFile)
 
 if __name__ == "__main__":
     main()
